4343f54920313326a8e81e9e0b9ae884611a15d5
Hereward Cooper <hereward.cooper@iomart.com>
2011-04-20 11:12:36 +0100

    first commit

---
+++ haproxy_check_ips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to check that the IPs HAProxy is listening on are also listed in the keepalived config
+
+HACONFIGFILE="/etc/haproxy/haproxy.cfg"
+KACONFIGFILE="/etc/keepalived/keepalived.conf"
+TEMPFILE=`mktemp`
+
+# Find all the 'listen' and 'frontend' lines from the haproxy configuration, and extract the IP address
+egrep '(listen|frontend)' $HACONFIGFILE | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | grep -v 127.0.0.1 > $TEMPFILE
+
+# Strip out repeat entries in the list of IPs then check for them in the keepalived config
+for i in `sort $TEMPFILE | uniq`; do
+	grep -q $i $KACONFIGFILE &&
+	echo "$i -- OK" ||
+	echo "$i -- NO MATCH";
+done
+
+# Tidy up
+rm -f $TEMPFILE
