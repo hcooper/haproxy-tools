f9c0a20bdac0983bd1e6afced4dcdf4da4022fe1
Hereward Cooper <hereward.cooper@iomart.com>
2012-02-08 20:35:53 +0000

    lots of changes - perfdata added, print strings reformatted, etc

--- check_mk/haproxy.py
+++ check_mk/haproxy.py
@@ -10,39 +10,25 @@ import os
 import re
 import sys
 
-# This is the command to run to retrieve the raw stats from the socket
-command="echo 'show stat' | nc -U /tmp/haproxy | egrep -v '(^#|^haproxystats)'"
+__version__ = "0.1"
+__author__ = "Hereward Cooper <coops@fawk.eu>"
+__website__ = "http://github.com/hcooper/haproxy-tools/"
 
-# These are the names haproxy calls each of the columns it output. We can improve the names
-# by just changing them here. But most of them are fine
-titles="pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,\
-        wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,\
-        pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,\
-        check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,\
-        hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,misc"
-
-# Put the column titles into an array
-title_array=titles.split(',')
-
-
-#---------------------
-# These are our checks
-#---------------------
-checks = [
-    ['rate', '100', '500'],
-    ['chkfail', '5', '25'],
-    ['status', '', '']      # status stays at the end, just for formatting purposes
-]
-
-
-# Convert the raw stats into nested arrays. Much nicer to use.
-# This functions creates an array, with each element being a dictonary of checks for each server
-# e.g.
-#  servers = [ {pxname: app1, rate: 15...}, {pxname: app2, rate: 7...} ]
 
 def build_array():
+
+    """
+    Convert the raw stats into nested arrays. Much nicer to use.
+    This functions creates an array, with each element being a dictonary of checks for each server
+    e.g. servers = [ {pxname: app1, rate: 15...}, {pxname: app2, rate: 7...} ]
+    """
+
     data = os.popen(command).read()     # Retrieve the raw data
+
+    #Initalize our array
+    global servers
     servers=[]
+
     for line in data.split('\n'):		# split out each line of raw input
         if re.match(r'^\s*$', line):    # skip empty lines
             continue
@@ -57,19 +43,27 @@ def build_array():
 
 
 def run_checks():
+
+    """
+    Interate through each server, and then each defined check, and compare the values
+    the the critical/warning levels, then alert if need be.
+    """
+
     for server in servers:
 
         if server['svname'] == "BACKEND" or server['svname'] == "FRONTEND":             # Skip FRONTEND and BACKEND lines
             continue
 
-        # Define some variables
+        # Define some variables before use
         result=""           # The complete set of results for each server's checks
         alert_warn=""       # Flag set if  check makes a server WARN
         alert_crit=""       #   "   "   "   "   "   "   "   "   CRIT
         alert_ok=""         #   "   "   "   "   "   "   "   "   OK
+        allperf=""          # The complete set of all performance data for a server
 
         for check,warn,crit in checks:
             output=""
+            perfdata=""
 
             # Special check for the "status" field as it's not a numeric value
             if check == "status":
@@ -91,16 +85,43 @@ def run_checks():
                 #if server[check] < warn:          # Disabled so OK doesn't give out stats 
                     #output += "| " + check + " OK " + server[check]
 
-            # Append the outcome of check check to the results line for the server
+                perfdata += check + "=" + server[check] + ";" + warn + ";" + crit + "|"
+
+            # Append the outcome of the check to the results line for the server
             result += output
+            allperf += perfdata
 
-        # Determine if we need to flag the server as OK, WARN or CRIT
-        if alert_warn==1:
-            print "1 " + "HAProxy_" + server['svname'] + " - WARNING - " + result
+        # If any of our checks have set the crit/warn flags, act on it
         if alert_crit==1:
-            print "2 " + "HAProxy_" + server['svname'] + " - CRITICAL - " + result
-        if alert_crit != 1 and alert_warn != 1:
-            print "0 " + "HAProxy_" + server['svname'] + " - OK " + result
+            print "2 HAProxy_%s %s CRITICAL - %s" % (server['svname'], allperf, result)
+        elif alert_warn==1:
+            print "1 HAProxy_%s %s WARNING - %s" % (server['svname'], allperf, result)
+        elif alert_crit != 1 and alert_warn != 1:
+            print "0 HAProxy_%s %s OK - %s" % (server['svname'], allperf, result)
 
-servers = build_array()
-run_checks()
+if __name__ == "__main__":
+
+    # This is the command to run to retrieve the raw stats from the socket
+    #command="echo 'show stat' | nc -U /tmp/haproxy | egrep -v '(^#|^haproxystats)'"
+    command="cat /tmp/hap.tmp"
+
+    # These are the names haproxy calls each of the columns it output. We can improve the names
+    # by just changing them here. But most of them are fine
+    titles="pxname,svname,qcur,qmax,scur,smax,slim,stot,bin,bout,dreq,dresp,ereq,econ,eresp,\
+        wretr,wredis,status,weight,act,bck,chkfail,chkdown,lastchg,downtime,qlimit,\
+        pid,iid,sid,throttle,lbtot,tracked,type,rate,rate_lim,rate_max,check_status,\
+        check_code,check_duration,hrsp_1xx,hrsp_2xx,hrsp_3xx,hrsp_4xx,hrsp_5xx,\
+        hrsp_other,hanafail,req_rate,req_rate_max,req_tot,cli_abrt,srv_abrt,misc"
+
+    # Put the column titles into an array
+    title_array=titles.split(',')
+
+    # These are our checks (the field, the warning level and the critical level)
+    checks = [
+        ['rate', '250', '500'],
+        ['chkfail', '5', '25'],
+        ['status', '', ''] # status stays at the end, just for formatting purposes
+    ]
+
+    build_array()
+    run_checks()
